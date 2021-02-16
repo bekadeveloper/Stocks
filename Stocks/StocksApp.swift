cebc3106478fd2dc42fd3523b40309bc8c442dd3
Begzod Bakhriddinov <beka083bakhriddinov@gmail.com>
2021-02-16 20:15:10 +0500

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Begzod Bakhriddinov on 16/02/21.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
